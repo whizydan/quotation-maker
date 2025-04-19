d649770f510be4cd958644cbe4d210f8bd4367af
whizydan <whizydan@gmail.com>
2025-04-19 18:39:05 +0300

    added tailwind animate and shadcn

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+import animate from "tailwindcss-animate";
+
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate],
+}
